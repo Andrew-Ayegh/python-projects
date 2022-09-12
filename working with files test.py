c835e739f5f8d61defe3b8ca127c7012cba527f7
Andrew Ayegh <andrewayegh2004@gmail.com>
2022-09-12 16:49:46 +0100

    Add files via upload

---
+++ working with files test.py
@@ -0,0 +1,15 @@
+# file = open("test.txt", "w")
+# file.write("Game\nof\nThrones")
+# file.close()
+
+# with open("test.txt") as f:
+#     print(f.read())
+
+
+# with open("game of thrones.txt") as f:
+#     for lines in f:
+#         words = lines.split()
+#         for word in words:
+#             print(word[0], end = "")
+
+print(5//2)
